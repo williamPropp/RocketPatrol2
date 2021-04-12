0e85a089ecd2c673a378a623674ea18be54e8046
William Propp <33852102+williamPropp@users.noreply.github.com>
2021-04-11 17:27:06 -0700

    quick update

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,11 +10,15 @@ class Play extends Phaser.Scene {
     preload() {
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/rocket.png');
-        this.load.image('starfield', './assets/starfield.png');
+        this.load.image('starfield', 'assets/starfield.png');
 
     }
 
     create() {
+
+        //Starfield
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+
         //Green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
 
@@ -24,11 +28,9 @@ class Play extends Phaser.Scene {
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 
-        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
-
     }
 
-    update() {
-        this.starfield.tilePositionX -= 4;
-    }
+    // update() {
+    //     this.starfield.tilePositionX -= 4;
+    // }
 }
