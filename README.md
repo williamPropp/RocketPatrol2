9783b16d98d6e3b7c14a8b3f94074d18a37001f7
William Propp <33852102+williamPropp@users.noreply.github.com>
2021-04-20 20:42:12 -0700

    added points breakdown and credits

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# RocketPatrol2
-a remake of the game Rocket Patrol in javascript
+# William Propp
+# Virus Patrol
+# 04/20/2021
+# Project Completion : 20 hours
+
+a modded remake of the game Rocket Patrol in javascript
+
+Modifications Implemented:
+- Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+- Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+- Implement parallax scrolling (10)
+- Create 4 new explosion SFX and randomize which one plays on impact (10) (*Randomized Clicks*)
+- Track a high score that persists across scenes and display it in the UI (5)
+- Add your own (copyright-free) background music to the Play scene (5) (*I made my own music*)
+- Implement the speed increase that happens after 30 seconds in the original game (5)
+ 
+Total: 115
+
+Cheat Codes:
+S: skip to 30 seconds to speed up the game
+G: skip to gameOver screen
+
+Assets Used:
+click sounds: https://freesound.org/people/joebro10/sounds/219318/
+
+Help:
+Ishaan helped lead me in the right direction on how to implement moving text
--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
-'use strict';
+// (This is all included in REAMDME.md)
+
+// # William Propp
+// # Virus Patrol
+// # 04/20/2021
+// # Project Completion : 20 hours
+
+// a modded remake of the game Rocket Patrol in javascript
+
+// Modifications Implemented:
+// (60) Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) 
+// (20) Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points 
+// (10) Implement parallax scrolling 
+// (10) Create 4 new explosion SFX and randomize which one plays on impact (*Randomized Clicks*)
+// (5)  Track a high score that persists across scenes and display it in the UI
+// (5)  Add your own (copyright-free) background music to the Play scene (*I made my own music*)
+// (5)  Implement the speed increase that happens after 30 seconds in the original game
+ 
+// Total: 115
+
+// Cheat Codes:
+// S: skip to 30 seconds to speed up the game
+// G: skip to gameOver screen
+
+// Assets Used:
+// click sounds: https://freesound.org/people/joebro10/sounds/219318/
+
+// Help:
+// Ishaan helped lead me in the right direction on how to implement moving text
 
 let config = {
     type: Phaser.Canvas,
@@ -19,10 +47,3 @@ let highScore = 0;
 
 //Reserve keyboard vars
 let keyF, keyR, keyG, keyS, keyLEFT, keyRIGHT;
-
-/*
-Assets Used:
-Sci-fi Texture: https://www.freepik.com/premium-vector/circuit-board-vector-seamless-texture-sci-fi-electronic-background_4080063.htm
-click sounds: https://freesound.org/people/joebro10/sounds/219318/
-Sountrack: I made it in Ableton
-*/
